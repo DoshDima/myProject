e1767e00988011d1f20648d863c3931176398028
DoshDima <140376594+DoshDima@users.noreply.github.com>
2023-07-28 12:01:03 +0300

    kelvinProject

---
+++ Kelvin/js/app.js
@@ -0,0 +1,29 @@
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-button');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+
+navBtn.onclick = () => {
+  if (nav.classList.toggle('open')) {
+    navBtnImg.src = "img/nav-close.svg";
+  } else {
+    navBtnImg.src = "img/nav-open.svg";
+  }
+}
+
+
+AOS.init({
+  disable: 'mobile',
+  once:true
+});
+
+
+
+
+
+
+
+
+
+
